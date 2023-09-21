219c417f4c6b51e05e30774c97f81d6a26a35fc8
faizGhazali <ahmadfaiz21@outlook.com>
2023-09-21 16:24:47 +0800

    some Changes

--- pages/index.js
+++ pages/index.js
@@ -2,46 +2,52 @@ import React, { useEffect,useRef } from 'react';
 import HEREMaps from '../components/mapComponents/Map';
 import OverlayComponent from '../components/mapComponents/OverlayComponent'
 import Header from '../components/Header';
+import Ws from '../helper/Websocket';
 
-const WebS =()=>{
+
+import { useRouter } from 'next/router';
+
+const connect = "ws://103.233.1.179:3000"
+
+const Homepage =()=>{
+  const router = useRouter();
   const dataRef = useRef();
   
+  const hendleProps = (prop) =>{
+    dataRef.current = prop;
+  }
+  const handleSignin = (formData) => {
+    // Handle the form data here
+    console.log('Received form data:', formData);
+  };
+  const handleHeader = (flag)=>{
+    if(flag=="Signed Out"){
+      console.log("hello world");
+      localStorage.removeItem('dummySigninData');
+      router.push('/singin');
+    } 
+  }
   useEffect(() => {
-      const socket = new WebSocket('ws://103.233.1.179:3000');
-  
-      socket.addEventListener('open', (event) => {
-        console.log('WebSocket is open:', event);
-        socket.send('Hello, server!');
-      });
-  
-      socket.addEventListener('message', (event) => {
-         // console.log('Message from server:', event);
-          dataRef.current = event.data;
-         
-      });
-  
-      socket.addEventListener('close', (event) => {
-        console.log('WebSocket is closed:', event);
-      });
-  
-      socket.addEventListener('error', (event) => {
-        console.error('WebSocket error:', event);
-      });
-  
-      return () => {
-        socket.close(); // Close the connection when component is unmounted
-      };
-    }, []);
+    const isLoggedIn = localStorage.getItem('dummySigninData'); 
+    if (!isLoggedIn) {
+      console.log("user not signed in");
+      router.push('/singin');
+    }else{
 
+    }
+  }, [router]);
+
+
+  
   return (
       <>
-       <Header />
+       <Header props={handleHeader}/>
+       <Ws props={hendleProps} connection={connect}/>
+      <HEREMaps data={dataRef} /> 
+      <OverlayComponent data={dataRef}/> 
       
-      
-      <HEREMaps data={dataRef} />
-      <OverlayComponent data={dataRef}/>
       </>
   )
 }
 
-export default WebS;
+export default Homepage;
